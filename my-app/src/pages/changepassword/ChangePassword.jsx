da27d1dd774adcceee4a2491e9cebd3c7bb030f5
Vanboizz <102446711+Vanboizz@users.noreply.github.com>
2023-02-27 08:49:30 +0700

    Handle Input Change

--- my-app/src/pages/changepassword/ChangePassword.jsx
+++ my-app/src/pages/changepassword/ChangePassword.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../changepassword/ChangePassword.css"
 
+const initial = {
+  newPassword: "",
+  retypeNewPassword: ""
+}
+
 const ChangePassword = () => {
+  const [state, setState] = useState(initial)
+  const { newPassword, retypeNewPassword } = state
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+    setState({ ...state, [name]: value })
+  }
+
   return (
     <div className='gridContainer'>
       <h1>Change Password</h1>
       <div className='gridContent'>
         <form action="">
-          <div style={{ position: "relative", width: "350px" }}>
-            <input type="password" placeholder='New Password' />
-            <button>
-              <svg stroke="currentColor" fill="currentColor" style={{ position: "absolute", right: "5%", top: "25%", fontSize: "1rem" }} strokeWidth="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M942.2 486.2C847.4 286.5 704.1 186 512 186c-192.2 0-335.4 100.5-430.2 300.3a60.3 60.3 0 0 0 0 51.5C176.6 737.5 319.9 838 512 838c192.2 0 335.4-100.5 430.2-300.3 7.7-16.2 7.7-35 0-51.5zM512 766c-161.3 0-279.4-81.8-362.7-254C232.6 339.8 350.7 258 512 258c161.3 0 279.4 81.8 362.7 254C791.5 684.2 673.4 766 512 766zm-4-430c-97.2 0-176 78.8-176 176s78.8 176 176 176 176-78.8 176-176-78.8-176-176-176zm0 288c-61.9 0-112-50.1-112-112s50.1-112 112-112 112 50.1 112 112-50.1 112-112 112z"></path></svg>
-            </button>
+          <div style={{ marginBottom: "1rem", width: "350px" }}>
+            <input type="password" placeholder='New Password' name='newPassword' value={newPassword || ""} onChange={handleInputChange} />
           </div>
-          <div style={{ position: "relative" }}>
-            <input type="password" placeholder='Re-type New Password' />
-            <button>
-              <svg stroke="currentColor" fill="currentColor" style={{ position: "absolute", right: "5%", top: "25%", fontSize: "1rem" }} strokeWidth="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M942.2 486.2C847.4 286.5 704.1 186 512 186c-192.2 0-335.4 100.5-430.2 300.3a60.3 60.3 0 0 0 0 51.5C176.6 737.5 319.9 838 512 838c192.2 0 335.4-100.5 430.2-300.3 7.7-16.2 7.7-35 0-51.5zM512 766c-161.3 0-279.4-81.8-362.7-254C232.6 339.8 350.7 258 512 258c161.3 0 279.4 81.8 362.7 254C791.5 684.2 673.4 766 512 766zm-4-430c-97.2 0-176 78.8-176 176s78.8 176 176 176 176-78.8 176-176-78.8-176-176-176zm0 288c-61.9 0-112-50.1-112-112s50.1-112 112-112 112 50.1 112 112-50.1 112-112 112z"></path></svg>
-            </button>
+          <div style={{ marginBottom: "1rem" }}>
+            <input type="password" placeholder='Re-type New Password' name='retypeNewPassword' value={retypeNewPassword || ""} onChange={handleInputChange} />
+
           </div>
         </form>
       </div>
