fb4bdf5484e8b023c2f129282a0026f494338395
Vanboizz <102446711+Vanboizz@users.noreply.github.com>
2023-03-08 15:47:49 +0700

    swiper

--- my-app/src/pages/home/Home.jsx
+++ my-app/src/pages/home/Home.jsx
@@ -2,56 +2,76 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getProducts } from '../../components/feature/products/productsSlice'
 import Slider from '../../components/slider/Slider'
-import "../home/Home.css"
 import { FaStar, FaPlusCircle } from 'react-icons/fa'
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/grid";
+import "../home/Home.css"
+import "swiper/css/navigation";
+import { Grid, Navigation } from "swiper"
 
 const Home = () => {
     const products = useSelector((state) => state.products)
+
     const dispatch = useDispatch()
     useEffect(() => {
         dispatch(getProducts())
     }, [])
 
     return (
-        <div>
+        <>
             <Slider />
-            <h2>THE MOST OUTSTANDING PHONE</h2>
-            <div className='gridContent'>
-                {
-                    products.data ? (
-                        products.data.map((value, index) => (
-                            <div className='item' key={index} style={{ position: "relative" }}>
-                                <div style={{ position: "absolute", top: "0", backgroundColor: "red", borderRadius: "0.4rem", padding: "0.2rem" }}>
-                                    <p style={{ color: "white", fontWeight: "600", fontSize: "0.8rem" }}>Giảm <span>{value.discount}%</span></p>
-                                </div>
-                                <div>
-                                    <img style={{ width: "14rem", height: "13rem", marginTop: "1rem" }} key={index} src={value.image[0]} alt="" />
-                                </div>
-                                <h3>{value.nameproducts}</h3>
-                                <div style={{ display: "flex", justifyContent: "space-between" }}>
-                                    <p style={{ color: "red", fontWeight: "bold", fontSize: "1.1rem" }}>{(value.size[0].pricesize * value.discount) / 100}<span>đ</span></p>
-                                    <p style={{ color: "#999", fontWeight: "bold", textDecoration: "line-through" }}>{value.size[0].pricesize}<span>đ</span></p>
-                                </div>
-                                <div style={{ display: "flex", justifyContent: "space-between" }}>
-                                    <div>
-                                        <FaStar style={{ color: "#FFD700", fontSize: "1.4rem" }} />
-                                        <FaStar style={{ color: "#FFD700", fontSize: "1.4rem" }} />
-                                        <FaStar style={{ color: "#FFD700", fontSize: "1.4rem" }} />
-                                        <FaStar style={{ color: "#FFD700", fontSize: "1.4rem" }} />
-                                        <FaStar style={{ color: "#FFD700", fontSize: "1.4rem" }} />
+            <div style={{ padding: "0 100px" }}>
+                <h2>THE MOST OUTSTANDING PHONE</h2>
+                <div className="product-list">
+                    <Swiper
+                        slidesPerView={5}
+                        navigation={true}
+                        grid={{
+                            rows: 2,
+                        }}
+                        spaceBetween={30}
+                        modules={[Grid, Navigation]}
+                        className="mySwiper"
+                    >
+                        {
+                            products.data ? products.data.map((value, index) => (
+                                <SwiperSlide key={index}>
+                                    <div className='item' key={index} >
+                                        <div className='discount'>
+                                            <p >Giảm <span>{value.discount}%</span></p>
+                                        </div>
+                                        <div className='url'>
+                                            <img key={index} src={value.image[0]} alt="" />
+                                        </div>
+                                        <h3>{value.nameproducts}</h3>
+                                        <div className='format'>
+                                            <p>{(value.size[0].pricesize * value.discount) / 100}&nbsp;đ</p>
+                                            <p>{value.size[0].pricesize}<span>đ</span></p>
+                                        </div>
+                                        <div className='promotion'>
+                                            {value.promotion}
+                                        </div>
+                                        <div className='icon'>
+                                            <div>
+                                                <FaStar className='star' />
+                                                <FaStar className='star' />
+                                                <FaStar className='star' />
+                                                <FaStar className='star' />
+                                                <FaStar className='star' />
+                                            </div>
+                                            <button>
+                                                <FaPlusCircle className='circle' />
+                                            </button>
+                                        </div>
                                     </div>
-                                    <button style={{ border: "none", backgroundColor: "transparent", cursor: "pointer" }}>
-                                        <FaPlusCircle style={{ color: "red", fontSize: "1.4rem" }} />
-                                    </button>
-                                </div>
-                            </div>
-                        ))
-                    ) :
-                        null
-                }
+                                </SwiperSlide>
+                            )) : null
+                        }
+                    </Swiper>
+                </div>
             </div>
-
-        </div>
+        </>
     )
 }
 
