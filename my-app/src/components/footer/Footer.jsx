86461b04849b08b80518e7c8ef5d7d68fd31cd2d
TuanKhang <tuankhang951@gmail.com>
2023-03-10 19:18:43 +0700

    complete footer and modify header

--- my-app/src/components/footer/Footer.jsx
+++ my-app/src/components/footer/Footer.jsx
@@ -1,9 +1,73 @@
 import React from 'react'
 import "../footer/Footer.css"
+import { FaGithub, FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='wrapper'>
 
+      <div className="line">
+      </div>
+
+      <div className="content">
+        <div className="list-1">
+
+          <div className="list-1__tspblack"></div>
+
+          <a className="list-1__component" href=''>
+              <FaGithub className='component__logo' />
+              <p>Github</p>
+            </a>
+
+          <br />
+          <a className="list-1__component" href=''>
+              <FaFacebook className='component__logo' />
+              <p>Facebook</p>
+            </a>
+
+          <br />
+          <a className="list-1__component" href=''>
+              <FaInstagram className='component__logo' />
+              <p>Instagram</p>
+            </a>
+
+          <br />
+          <a className="list-1__component" href=''>
+              <FaYoutube className='component__logo' />
+              <p>Youtube</p>
+            </a>
+
+        </div>
+
+        <div className="list-2">
+          <h2 className='list-2__title'>Products</h2>
+          <a href="" className='list-2__title__item'>Iphone</a>
+          <br />
+          <a href="" className='list-2__title__item'>Samsung Galaxy</a>
+          <br />
+          <a href="" className='list-2__title__item'>Realme</a>
+          <br />
+          <a href="" className='list-2__title__item'>Vivo</a>
+          <br />
+          <a href="" className='list-2__title__item'>Oppo</a>
+          <br />
+          <a href="" className='list-2__title__item'>Other</a>
+        </div>
+
+        <div className="list-3">
+          <h2 className='list-3__title'>Contact</h2>
+
+          <div className='list-3__title__item'>
+            Email: 
+            <a  href='' className=' list-3__title__email'> example@email.com</a>
+          </div>
+          <br />
+          <p  className='list-3__title__item'>Hotline: 1900 0000</p>
+          <br />
+          <p  className='list-3__title__item'>Address: 540 ap Tay, Tan Thanh , Thanh Binh, Dong Thap</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- my-app/src/components/header/Header.jsx
+++ my-app/src/components/header/Header.jsx
@@ -10,35 +10,38 @@ const Header = () => {
       </div>
 
       <div className="search">
-        <FaSearch className="icon-search" />
+        <FaSearch className="search__icon-search" />
 
-        <input className='input-search' type="text" placeholder="Tìm gì đó trên Smart Phone..." />
+        <input className='search__input-search' type="text" placeholder="Tìm gì đó trên Smart Phone..." />
       </div>
 
       <div className="list-option">
-        
-        <div className='option'>
-          <div className="icon-home home"></div>
+
+        <div className='list-option__option'>
+          <div className="option__icon-home option__home"></div>
           <a>Home</a>
         </div>
 
-        <div className='option'>
-          <div className="icon-phone phone"></div>
+        <div className='list-option__option'>
+          <div className="option__icon-phone option__phone"></div>
           <a>Phone</a>
         </div>
 
-        <div className='option'>
-          <div className="icon-pen pen"></div>
+        <div className='list-option__option'>
+          <div className="option__icon-pen option__pen"></div>
           <a>About Us</a>
         </div>
 
-        <div className='option'>
-          <div className="icon-cart cart"></div>
+        <div className='list-option__option'>
+          <div className="option__icon-cart option__cart option_contain-circle">
+            <div className="circle-yellow">0</div>
+
+          </div>
           <a>Cart</a>
         </div>
 
-        <div className='option'>
-          <div className="icon-user user"></div>
+        <div className='list-option__option'>
+          <div className="option__icon-user option__user"></div>
           <a>Login</a>
         </div>
       </div>
