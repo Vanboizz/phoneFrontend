73b48f0c126e3b556c95684bf60d4f8e350c35fd
Vanboizz <102446711+Vanboizz@users.noreply.github.com>
2023-10-03 13:59:18 +0700

    complete evaluate

--- my-app/src/components/modalreview/ModalReview.jsx
+++ my-app/src/components/modalreview/ModalReview.jsx
@@ -1,25 +1,53 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "../modalreview/ModalReview.css"
 import { AiOutlineClose, AiFillCamera, AiOutlineCloseCircle } from "react-icons/ai"
 import { FaStar } from 'react-icons/fa'
+import axios from 'axios'
 
-const ratingLabels = ["Very bad", "Bad", "Normal", "Good", "Great"]
-const perfomanceLabels = ["Lag", "Unstable", "Stable", "Strong", "Super strong"]
-const batteryLifeLabels = ["Very weak", "Weak", "Enough to use", "Terrible", "Extremely terrible"]
+const ratingLabels = [
+    { number: 1, value: "Very bad" },
+    { number: 2, value: "Bad" },
+    { number: 3, value: "Normal" },
+    { number: 4, value: "Good" },
+    { number: 5, value: "Great" }
+]
+const perfomanceLabels = [
+    { number: 1, value: "Lag" },
+    { number: 2, value: "Unstable" },
+    { number: 3, value: "Stable" },
+    { number: 4, value: "Strong" },
+    { number: 5, value: "Super strong" }
+];
+const batteryLifeLabels = [
+    { number: 1, value: "Very weak" },
+    { number: 2, value: "Weak" },
+    { number: 3, value: "Enough to use" },
+    { number: 4, value: "Terrible" },
+    { number: 5, value: "Extremely terrible" }
+]
 const cameraQuantityLabels =
-    ["Not taken well", "Not taken well", "Fine", "Beautiful capture", "Beautiful, professional photography"]
+    [
+        { number: 1, value: "Not taken well" },
+        { number: 2, value: "Not taken well" },
+        { number: 3, value: "Fine" },
+        { number: 4, value: "Beautiful capture" },
+        { number: 5, value: "Beautiful, professional photography" }
+    ]
 
 const ModalReview = (props) => {
-    const { productsById, isModal, setIsModal } = props
+    const { getListEvaluate, productsById, isModal, setIsModal } = props
     const [selectedImages, setSelectedImages] = useState([]);
-    const [selectedRating, setSelectedRating] = useState(5)
-    const [selectedPerformance, setSelectedPerformance] = useState(5)
-    const [selectedBatteryLife, setSelectedBatteryLife] = useState(5)
-    const [selectedCameraQuanlity, setSelectedCameraQuanlity] = useState(5)
+    const [selectedRating, setSelectedRating] = useState(ratingLabels[4])
+    const [selectedPerformance, setSelectedPerformance] = useState(perfomanceLabels[4])
+    const [selectedBatteryLife, setSelectedBatteryLife] = useState(batteryLifeLabels[4])
+    const [selectedCameraQuanlity, setSelectedCameraQuanlity] = useState(cameraQuantityLabels[4])
     const [hoverRating, setHoverRating] = useState(0);
-    const [hoverPerformance, setHoverPerformance] = useState(0);
-    const [hoverBatteryLife, setHoverBatteryLife] = useState(0);
-    const [hoverCameraQuantity, setHoverCameraQuantity] = useState(0);
+    const [hoverPerformance, setHoverPerformance] = useState("");
+    const [hoverBatteryLife, setHoverBatteryLife] = useState("");
+    const [hoverCameraQuantity, setHoverCameraQuantity] = useState("");
+    const [text, setText] = useState("")
+    const accessToken = localStorage.getItem("accessToken")
+    const [preview, setPreview] = useState([])
 
     const handleCloseModal = () => {
         setIsModal(false)
@@ -34,7 +62,7 @@ const ModalReview = (props) => {
     };
 
     const handlePerformanceMouseLeave = () => {
-        setHoverPerformance(0);
+        setHoverPerformance("");
     };
 
     const handlePerformanceMouseEnter = (star) => {
@@ -42,7 +70,7 @@ const ModalReview = (props) => {
     };
 
     const handleBatteryLifeMouseLeave = () => {
-        setHoverBatteryLife(0);
+        setHoverBatteryLife("");
     };
 
     const handleBatteryLifeMouseEnter = (star) => {
@@ -50,7 +78,7 @@ const ModalReview = (props) => {
     };
 
     const handleCameraQuantityMouseLeave = () => {
-        setHoverCameraQuantity(0);
+        setHoverCameraQuantity("");
     };
 
     const handleCameraQuantityMouseEnter = (star) => {
@@ -61,8 +89,8 @@ const ModalReview = (props) => {
         setSelectedRating(selectedRating)
     }
 
-    const handlePerformanceClick = (selectedPerformance) => {
-        setSelectedPerformance(selectedPerformance);
+    const handlePerformanceClick = (star) => {
+        setSelectedPerformance(star);
     };
 
     const handleBatteryLifeClick = (selectedBatteryLife) => {
@@ -73,19 +101,27 @@ const ModalReview = (props) => {
         setSelectedCameraQuanlity(selectedCameraQuanlity);
     };
 
+    const handleChangeText = (e) => {
+        e.preventDefault()
+        setText(e.target.value)
+    }
 
     const handleImageChange = (e) => {
-        const files = e.target.files;
-        const imageArray = [];
-
-        for (let i = 0; i < files.length; i++) {
-            const file = files[i];
-            const url = URL.createObjectURL(file);
-            imageArray.push(url);
+        const fileObj = []
+        let files = e.target.files
+        fileObj.push(files)
+        let reader;
+        for (var i = 0; i < fileObj[0].length; i++) {
+            reader = new FileReader()
+            reader.readAsDataURL(fileObj[0][i])
+            reader.onload = event => {
+                preview.push(event.target.result)
+                setPreview([... new Set(preview)])
+            }
         }
 
-        if (selectedImages.length + imageArray.length <= 3) {
-            setSelectedImages(prevSelectedImages => [...prevSelectedImages, ...imageArray]);
+        if (selectedImages.length + preview.length <= 3) {
+            setSelectedImages(prevSelectedImages => [...prevSelectedImages, ...preview]);
         }
     };
 
@@ -96,6 +132,34 @@ const ModalReview = (props) => {
             return newSelectedImages;
         });
     }
+
+    const handleSubmitForm = (e) => {
+        e.preventDefault();
+
+        const data = {
+            idproducts: productsById.data.idproducts,
+            starnumber: selectedRating,
+            review: text,
+            images: preview,
+            performance: selectedPerformance,
+            batterylife: selectedBatteryLife,
+            cameraquantity: selectedCameraQuanlity
+        }
+        axios.post("http://localhost:8000/evaluate/addEvaluate", data, {
+            headers: {
+                "Authorization": "Bearer " + accessToken
+            },
+        })
+            .then(response => {
+                setIsModal(false)
+                getListEvaluate();
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+
     return (
         <div className='modal-review is-active'>
             <div
@@ -128,11 +192,11 @@ const ModalReview = (props) => {
                         }
                     </h3>
                 </div>
-                <form action="" >
+                <form action="" onSubmit={handleSubmitForm}>
                     <div style={{ color: "#111", fontSize: "16px", fontWeight: "bold", padding: "15px", }}>General Assessment</div>
                     <div style={{ display: "flex", justifyContent: "space-evenly", width: "100%", borderBottom: "1px solid rgba(145,158,171,.239)", padding: "10px 0" }}>
                         {
-                            [1, 2, 3, 4, 5].map((star, index) => (
+                            ratingLabels.map((star, index) => (
                                 <div
                                     key={index}
                                     style={{ textAlign: "center", lineHeight: "1.5", cursor: "pointer" }}
@@ -141,9 +205,9 @@ const ModalReview = (props) => {
                                     onClick={() => handleRatingClick(star)}
                                 >
                                     <FaStar
-                                        style={{ color: star <= (hoverRating || selectedRating) ? "#ffbf00" : "#ccc", fontSize: "24px" }}
+                                        style={{ color: index <= ratingLabels.indexOf(hoverRating || selectedRating) ? "#ffbf00" : "#ccc", fontSize: "24px" }}
                                     />
-                                    <p style={{ fontSize: "12px" }}>{ratingLabels[star - 1]}</p>
+                                    <p style={{ fontSize: "12px" }}>{star.value}</p>
                                 </div>
                             ))
                         }
@@ -155,27 +219,29 @@ const ModalReview = (props) => {
                         <div style={{ display: "flex", padding: "15px", justifyContent: "space-between", alignItems: "center" }}>
                             <p style={{ fontSize: "13px", width: "50%" }}>Performance</p>
                             <div style={{ display: "flex", gap: "12px" }}>
-                                {[1, 2, 3, 4, 5].map((star, index) => (
-                                    <FaStar
-                                        key={index}
-                                        style={{ color: star <= (hoverPerformance || selectedPerformance) ? "#ffbf00" : "#ccc", fontSize: "16px", cursor: "pointer" }}
-                                        onMouseEnter={() => handlePerformanceMouseEnter(star)}
-                                        onMouseLeave={handlePerformanceMouseLeave}
-                                        onClick={() => handlePerformanceClick(star)}
-                                    />
-                                ))}
+                                {
+                                    perfomanceLabels.map((star, index) => (
+                                        <FaStar
+                                            key={index}
+                                            style={{ color: index <= perfomanceLabels.indexOf(hoverPerformance || selectedPerformance) ? "#ffbf00" : "#ccc", fontSize: "16px", cursor: "pointer" }}
+                                            onMouseEnter={() => handlePerformanceMouseEnter(star)}
+                                            onMouseLeave={handlePerformanceMouseLeave}
+                                            onClick={() => handlePerformanceClick(star)}
+                                        />
+                                    ))
+                                }
                             </div>
                             <div style={{ fontSize: "13px", width: "110px", textAlign: "left" }}>
-                                {perfomanceLabels[selectedPerformance - 1]}
+                                {selectedPerformance.value}
                             </div>
                         </div>
                         <div style={{ display: "flex", padding: "15px", justifyContent: "space-between", alignItems: "center" }}>
                             <p style={{ fontSize: "13px", width: "50%" }}>Battery life</p>
                             <div style={{ display: "flex", gap: "12px" }}>
-                                {[1, 2, 3, 4, 5].map((star, index) => (
+                                {batteryLifeLabels.map((star, index) => (
                                     <FaStar
                                         key={index}
-                                        style={{ color: star <= (hoverBatteryLife || selectedBatteryLife) ? "#ffbf00" : "#ccc", fontSize: "16px", cursor: "pointer" }}
+                                        style={{ color: index <= batteryLifeLabels.indexOf(hoverBatteryLife || selectedBatteryLife) ? "#ffbf00" : "#ccc", fontSize: "16px", cursor: "pointer" }}
                                         onMouseEnter={() => handleBatteryLifeMouseEnter(star)}
                                         onMouseLeave={handleBatteryLifeMouseLeave}
                                         onClick={() => handleBatteryLifeClick(star)}
@@ -183,16 +249,16 @@ const ModalReview = (props) => {
                                 ))}
                             </div>
                             <div style={{ fontSize: "13px", width: "110px", textAlign: "left" }}>
-                                {batteryLifeLabels[selectedBatteryLife - 1]}
+                                {selectedBatteryLife.value}
                             </div>
                         </div>
                         <div style={{ display: "flex", padding: "15px", justifyContent: "space-between", alignItems: "center" }}>
                             <p style={{ fontSize: "13px", width: "50%" }}>Camera quality</p>
                             <div style={{ display: "flex", gap: "12px" }}>
-                                {[1, 2, 3, 4, 5].map((star, index) => (
+                                {cameraQuantityLabels.map((star, index) => (
                                     <FaStar
                                         key={index}
-                                        style={{ color: star <= (hoverCameraQuantity || selectedCameraQuanlity) ? "#ffbf00" : "#ccc", fontSize: "16px", cursor: "pointer" }}
+                                        style={{ color: index <= cameraQuantityLabels.indexOf(hoverCameraQuantity || selectedCameraQuanlity) ? "#ffbf00" : "#ccc", fontSize: "16px", cursor: "pointer" }}
                                         onMouseEnter={() => handleCameraQuantityMouseEnter(star)}
                                         onMouseLeave={handleCameraQuantityMouseLeave}
                                         onClick={() => handleCameraQuantityClick(star)}
@@ -200,17 +266,17 @@ const ModalReview = (props) => {
                                 ))}
                             </div>
                             <div style={{ fontSize: "13px", width: "110px", textAlign: "left" }}>
-                                {cameraQuantityLabels[selectedCameraQuanlity - 1]}
+                                {selectedCameraQuanlity.value}
                             </div>
                         </div>
                     </div>
                     <div style={{ padding: "13px" }}>
-                        <textarea style={{ maxHeight: "40vh", margin: "10px 0", borderRadius: "8px", resize: "vertical", display: "block", maxWidth: "100%", minWidth: "100%", padding: "13px", minHeight: "8em" }} placeholder='Please share some thoughts about the product'></textarea>
+                        <textarea value={text} name='review' onChange={handleChangeText} style={{ maxHeight: "40vh", margin: "10px 0", borderRadius: "8px", resize: "vertical", display: "block", maxWidth: "100%", minWidth: "100%", padding: "13px", minHeight: "8em" }} placeholder='Please share some thoughts about the product' required></textarea>
                     </div>
                     <div style={{ display: "flex", padding: "13px" }}>
-                        {selectedImages.length > 0 && (
+                        {preview.length > 0 && (
                             <div style={{ display: "flex", gap: "30px", alignItems: "center" }}>
-                                {selectedImages.map((imageUrl, index) => (
+                                {(preview || []).map((imageUrl, index) => (
                                     <div key={index} style={{ position: "relative" }}>
                                         <img style={{ maxWidth: "100px", maxHeight: "70px", borderRadius: "5px" }} src={imageUrl} alt={`Selected Image ${index}`} />
                                         <div style={{ cursor: "pointer", position: "absolute", top: "0", left: "100%" }} onClick={() => handleDeleteImage(index)}>
@@ -223,7 +289,7 @@ const ModalReview = (props) => {
                         )}
                         {selectedImages.length < 3 && (
                             <div>
-                                <input style={{ display: "none" }} type="file" id='image' multiple="multiple" accept="image/x-png,image/gif,image/jpeg" onChange={handleImageChange} />
+                                <input style={{ display: "none" }} name="file" type="file" id='image' multiple="multiple" onChange={handleImageChange} required />
                                 <label htmlFor="image" style={{ border: "1px dashed rgba(145,158,171,.239)", borderRadius: "8px", color: "#637381", cursor: "pointer", fontSize: "13px", fontWeight: "bold", gap: "10px", padding: "7px 11px", whiteSpace: "nowrap", margin: "0 0.5rem", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
                                     <AiFillCamera fontSize={24} />
                                     <div style={{ height: "24px", marginTop: "3px", lineHeight: "1.5" }}>
@@ -234,7 +300,7 @@ const ModalReview = (props) => {
                         )}
                     </div>
                     <div style={{ position: "sticky", bottom: "0", width: "100%", zIndex: "400", backgroundColor: "white" }}>
-                        <button style={{ border: "none", backgroundColor: "#1a94ff", color: "white", fontWeight: "bold", textAlign: "center", margin: "10px auto", padding: "10px 30px", borderRadius: "5px", width: "95%", cursor: "pointer" }}>SUBMIT REVIEW</button>
+                        <button type='submit' style={{ border: "none", backgroundColor: "#1a94ff", color: "white", fontWeight: "bold", textAlign: "center", margin: "10px auto", padding: "10px 30px", borderRadius: "5px", width: "95%", cursor: "pointer" }}>SUBMIT REVIEW</button>
                     </div>
                 </form>
             </div>
